ed927d1ac83a8cc8bcf80b91186be7a84a0035ae
Gabi <gabriela.nani22092008@gmail.com>
2022-06-02 20:36:35 +0300

    configurare

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const path = require("path")
 
 module.exports = {
@@ -8,5 +12,28 @@ module.exports = {
         filename: "bundle.[hash].js",
         path: path.resolve(__dirname, "dist")
     },
-
+    resolve: {
+        extensions: [".js"],
+        alias: {
+            //import ../../../core/components
+            //import @core/components
+            "@": path.resolve(__dirname, "src"),
+            "@core": path.resolve(__dirname, "src/core")
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "index.html"
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, "src/favicon.ico"),
+                    to: path.resolve(__dirname, "dist") },
+            ],
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "bundle.[hash].css"
+        }),
+    ],
 }
