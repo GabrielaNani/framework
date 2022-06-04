81d42048aca56a78e9c72bf61566685841014bdb
Gabi <gabriela.nani22092008@gmail.com>
2022-06-04 10:02:59 +0300

    Dev/Prod

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const CopyPlugin = require("copy-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const path = require("path")
+const isProd = process.env.NODE_env === "production"
+const isDev =  !isProd
 
 module.exports = {
     context: path.resolve(__dirname, "src"),
