b054b4e8da54f674b707fcfed46c96dba8ac764d
Gabi <gabriela.nani22092008@gmail.com>
2022-07-19 19:09:21 +0300

    js

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -15,35 +15,58 @@ export class Table extends ExcelComponent {
   //   console.log("click")
   // }
   onMousedown(ev) {
+    let value
+    console.log(value);
     if (ev.target.dataset.resize) {
       const $resizer = $(ev.target)
       const type = $resizer.data.resize
-      console.log(type);
-      // const $parent = $resizer.$element.parentNode // bad!
-      // const $parent = $resizer.$element.closest(".column")//betterbutnotbest
+      // // const $parent = $resizer.$element.parentNode // bad!
+      // eslint-disable-next-line max-len
+      // // const $parent = $resizer.$element.closest(".column")//betterbutnotbest
       const $parent = $resizer.closest("[data-type = \"resizable\"]")
-      const cells = this.$root.findAll(`[data-col = "${$parent.data.col}"]`)
       const cords = $parent.getCords()
-      console.log(cords)
+      $resizer.css({
+        opacity: 1,
+        zIndex: 1000,
+        bottom: "-500px"
+      })
       document.onmousemove = ev => {
         if (type === "column") {
           const delta = ev.pageX - cords.right
-          const value = cords.width + delta
-          // $parent.$element.style.width = value + "px"
-          $parent.css({
-            width: value + "px"
+          value = cords.width + delta
+          $resizer.css({
+            right: `${-delta}px`
           })
-          cells.forEach(element => element.style.width = value + "px")
-        } else {
-          const delta = ev.pageY - cords.bottom
-          const value = cords.height + delta
-          $parent.css({
-            height: value + "px",
-          })
+          // // $parent.$element.style.width = value + "px"
+          // $parent.css({
+          //   width: value + "px"
+          // })
+          // cells.forEach(element => element.style.width = value + "px")
+        // } else {
+        //   const delta = ev.pageY - cords.bottom
+        //   value = cords.height + delta
+        //   // $parent.css({
+        //   //   height: value + "px"
+        //   // })
         }
       }
       document.onmouseup = () => {
         document.onmousemove = null
+        document.onmouseup = null
+        if (type === "column") {
+          $parent.css({
+            width: value + "px"
+          })
+          // eslint-disable-next-line max-len
+          this.$root.findAll(`[data-col = "${$parent.data.col}"]`).forEach(element => element.style.width = value + "px")
+          $resizer.css({
+            opacity: 0,
+            bottom: 0,
+            right: 0
+          })
+        } else {
+
+        }
       }
     }
   }
